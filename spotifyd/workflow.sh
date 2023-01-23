60fafab86e7e10ed005d858fc5b9501a4424d8e3
Amann Malik <amannmalik@gmail.com>
2023-01-22 21:22:43 -0600

    tweak

--- spotifyd/workflow.sh
+++ spotifyd/workflow.sh
@@ -2,12 +2,12 @@
 set -euo
 
 function build() {
-  local source_vm_folder="source"
+  local images_folder="images"
   local vm_folder="machine"
   local output_folder="output"
-  ../builder/workflow.sh build "${source_vm_folder}"
+  ../builder/workflow.sh build "${images_folder}"
   packer build -parallel-builds=1 \
-    -var "images_folder=${source_vm_folder}" \
+    -var "images_folder=${images_folder}" \
     -var "vm_name=${vm_folder}" \
     -var "vm_folder=${vm_folder}" \
     -var "output_folder=${output_folder}" .
